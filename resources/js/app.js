2b8951ca8e3695139868e0cac739489887cf5848
chienBv92 <mcnb.02@gmail.com>
2019-11-04 20:29:22 +0700

    add code all

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+require('./bootstrap');
+//window.Vue = require('vue');
+import Vue from 'vue';
+import App from  '../component/App';
+
+Vue.config.devtools = true;
+//Vue.component('app-component', require('../component/App').default())
+
+const app = new Vue({
+    el: '#app',
+    components: {App}
+
+})
---
+++ storage/framework/views/1b8249da4aadd8db64727c481fb58669f52d3525.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(app()->getLocale()); ?>">
+<head>
+    <title>Laravel</title>
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+</head>
+<body>
+<div id="app">
+    <app></app>
+
+</div>
+<script src="/js/app.js"></script>
+
+</body>
+</html>
+<?php /**PATH D:\PHP\code\demo02\resources\views/welcome.blade.php ENDPATH**/ ?>
